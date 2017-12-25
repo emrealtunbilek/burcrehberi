a0e93868a78603e38a28e17f205dd9fc995bd301
Emre Altunbilek <emrealtunbilek@gmail.com>
2017-12-25 16:17:27 +0300

    custom array adapter 1. bölüm anlatıldı

---
+++ app/src/main/java/com/emrealtunbilek/burcrehberiapp/BurclarArrayAdapter.kt
@@ -0,0 +1,16 @@
+package com.emrealtunbilek.burcrehberiapp
+
+import android.content.Context
+import android.widget.ArrayAdapter
+
+/**
+ * Created by Emre on 25.12.2017.
+ */
+class BurclarArrayAdapter(context: Context?, resource: Int, textViewResourceId: Int, burcAdlari: Array<String>, burcTarihleri:Array<String>, burcResimleri:Array<Int>)
+    : ArrayAdapter<String>(context, resource, textViewResourceId, burcAdlari) {
+
+
+
+
+
+}
--- app/src/main/java/com/emrealtunbilek/burcrehberiapp/MainActivity.kt
+++ app/src/main/java/com/emrealtunbilek/burcrehberiapp/MainActivity.kt
@@ -1,49 +1,26 @@
 package com.emrealtunbilek.burcrehberiapp
 
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.TextView
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    var burclar= arrayOf("Koç","Boğa","İkizler","Yengeç","Aslan","Başak","Terazi","Akrep","Yay","Oğlak","Kova","Balık")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-      // var burclarAdapter=ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,burclar)
-        var burclarAdapter=ArrayAdapter<String>(this, R.layout.tek_satir, R.id.tvBurcAdi, burclar)
+        var burclar = resources.getStringArray(R.array.burclar)
+        var burcTarihleri = resources.getStringArray(R.array.burcTarih)
+        var burcResimleri = arrayOf(R.drawable.koc1, R.drawable.boga2, R.drawable.ikizler3, R.drawable.yengec4, R.drawable.aslan5, R.drawable.basak6,
+                R.drawable.terazi7, R.drawable.akrep8, R.drawable.yay9, R.drawable.oglak10, R.drawable.kova11, R.drawable.balik12)
 
-        listBurclar.adapter=burclarAdapter
-
-        /*listBurclar.setOnItemClickListener(object : AdapterView.OnItemClickListener {
-            override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
-                var gecici=view as TextView
-
-                Toast.makeText(this@MainActivity, "Tıklanıldı : "+gecici.text.toString()+ " Pos:"+position,Toast.LENGTH_SHORT).show()
-
-            }
-
-        })*/
-
-        listBurclar.setOnItemClickListener { parent, view, position, id ->
-
-            var gecici=view as TextView
-
-            Toast.makeText(this@MainActivity, "Tıklanıldı : "+gecici.text.toString()+ " Pos:"+position,Toast.LENGTH_SHORT).show()
-
-        }
-
-
+        //var myAdapter=ArrayAdapter<String>(this,R.layout.tek_satir, R.id.tvBurcAdi, burclar)
+        var myAdapter=BurclarArrayAdapter(this, R.layout.tek_satir,R.id.tvBurcAdi, burclar, burcTarihleri,burcResimleri)
+        listBurclar.adapter=myAdapter
 
     }
 
